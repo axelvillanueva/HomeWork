eab2d737d063d270d707625dda8d1987e7b75fb9
axelvillanueva <axelvillanueva36@gmail.com>
2021-07-02 00:07:55 -0300

    added layout of antd

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sidebar from './layout/sidebar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar>
+        <div className="">Mis tareas de hoy</div>
+      </Sidebar>
+    </div>
+  );
+}
+
+export default App;
